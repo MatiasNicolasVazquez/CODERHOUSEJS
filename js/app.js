6002f107f630182b3629c49592fc5d1475f2a6d7
matias.vazquez <mnvm2509@gmail.com>
2022-04-14 18:00:59 -0300

    Desafio clase 4

--- js/app.js
+++ js/app.js
@@ -1,23 +1,37 @@
-console.log("hola mundo");
-let entrada = parseInt(prompt("¡Ingrese un numero por favor!"));
-/* Se ingresa un numero al entrar a la pagina web. En cada iteración del ciclo se le suma uno,
- y posteriormente se lo multiplica por 2. Los resultados a mostrarse si se ingrasara un 1 son: 4 6 8 10 12 14 16 18 20 22.
- Para el uso de condicional, meti un if de que si las cuentas mencionadas anteriormente dan como resultado 10, no se muestre*/
-for (i = 0; i < 10; i++) {
-    entrada = entrada + 1;
-    let resultado = entrada * 2;
-    if(resultado==10){
-        continue;}
-    alert(resultado);
+console.log("Hola mundo");
+
+function agregarTarea(a) {
+    if (a == 1) {
+        let fisica = prompt("Ingrese el nombre de la tarea");
+        let comienzo = parseInt(prompt("Ingrese el horario estimado de comienzo (formato 24hs (sin los dos puntos))"));
+        let duracion = parseInt(prompt("Ingrese la duracion estimada de la actividad en minutos"));
+        let final = comienzo + duracion;
+        alert("Tarea fisica agendada: " + fisica + ". Horario de comienzo: " + comienzo + "hs. Horario estimado de finalizacion: " + final + "hs.");
+
+    }
+    else if(a==2){
+        let hogar = prompt("Ingrese el nombre de la tarea");
+        let comienzo = parseInt(prompt("Ingrese el horario estimado de comienzo (formato 24hs (sin los dos puntos))"));
+        let duracion = parseInt(prompt("Ingrese la duracion estimada de la actividad en minutos"));
+        let final = comienzo + duracion;
+        alert("Tarea hogareña agendada: " + hogar + ". Horario de comienzo: " + comienzo + "hs. Horario estimado de finalizacion: " + final + "hs.");
+    }
+    else if(a==3){
+        let estudios = prompt("Ingrese el nombre de la tarea");
+        let comienzo = parseInt(prompt("Ingrese el horario estimado de comienzo (formato 24hs (sin los dos puntos))"));
+        let duracion = parseInt(prompt("Ingrese la duracion estimada de la actividad en minutos"));
+        let final = comienzo + duracion;
+        alert("Tarea relacionada al estudio agendada: " + estudios + ". Horario de comienzo: " + comienzo + "hs. Horario estimado de finalizacion: " + final + "hs.");
+    }
+
+    else {
+        alert("Actividad no valida");
+    }
 }
-const blanco=" ";
-let oracion="";
 
-let palabra=prompt("Ingrese una palabra");
-while(palabra!="basta"){
-    oracion= oracion + blanco + palabra;
-    alert(oracion);
-    palabra=prompt("Ingrese otra palabra");
+let tareas = parseInt(prompt("¿Cuantas tareas desea agregar a la lista?"));
 
+for(i=1;i<=tareas;i++){
+let actividad = parseInt(prompt("¿Que tipo de actividad desea añadir a la lista? (1 física, 2 hogareña, 3 estudios) "));
+agregarTarea(actividad);    
 }
-
